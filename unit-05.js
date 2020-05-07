6ab97a664bc8c4f4475b6259147d7cffa3f97974
NHN <58180674+nhn6298@users.noreply.github.com>
2020-05-07 10:41:38 +0700

    Create unit-05.js

---
+++ unit-05.js
@@ -0,0 +1,14 @@
+/**
+ * Sử dụng kiến thức đã học khai báo object person gồm các property name, age, gender. 
+ * Ví dụ:
+ *  var person = {
+ *    name: 'Pham Thinh',
+ *    age: 25,
+ *    gender: 'male'
+ *  };
+ */
+var person = {
+  name: 'Kim Trang',
+  age: 22,
+  gender: 'female'
+};
