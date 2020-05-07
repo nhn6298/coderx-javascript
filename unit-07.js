a2fb7b2140529ca9c73a54a409f44ea4248ea950
NHN <58180674+nhn6298@users.noreply.github.com>
2020-05-07 10:45:05 +0700

    Create unit-07.js

---
+++ unit-07.js
@@ -0,0 +1,38 @@
+-------- exercise 1 ----------
+var a = 10;
+var h = 20;
+/**
+ * a: cạnh đáy của 1 hình tam giác
+ * h: chiều cao của tam giác đó
+ * Yêu cầu: khai báo biến s bằng diện tích của hình tam giác này 
+ */
+// viết code ở đây.
+var s = a * h / 2
+-------- exercise 2 ----------
+var width = 10;
+var height = 20;
+/**
+ * width: Chiều rộng của 1 hình chữ nhật
+ * height: Chiều dài của hình chữ nhật
+ * Yêu cầu: 
+ * - Khai báo biến s là diện tích của hình chữ nhật này.
+ * - Khai báo biến p là chu vi của hình chữ nhật này.
+ */
+// viết code ở đây.
+var s = width * height;
+var p = (width + height) * 2;
+-------- exercise 3 ----------
+/**
+ * r: Bán kính của 1 hình tròn
+ * s: Diện tích của hình tròn đó
+ * pi: 3.14
+ * Yêu cầu: 
+ * - Khai báo biến p là chu vi của hình tròn này.
+ * - Khao báo biến s là diện tích của hình tròn này.
+ */
+// viết code ở đây.
+var r = 4;
+var p = 2 * r * 3.14;
+var s = r * r * 3.14;
+
+-------- finish ----------
