daed1039c812e9f0beec55f480a0037a2e672a93
NHN <58180674+nhn6298@users.noreply.github.com>
2020-05-07 11:35:37 +0700

    Create unit-19.js

---
+++ unit-19.js
@@ -0,0 +1,27 @@
+//-------- exercise 1 ----------
+// Viết hàm trả về số chẵn đầu tiên trong mảng sử dụng find method
+// Nếu không tìm thấy số chẵn nào thì trả về undefined
+function findFirstEvenNumber(array) {
+  // your code here
+  return array.find(function(x){
+      return x % 2 === 0;
+  })
+}
+//-------- exercise 2 ----------
+/**
+ * viết hàm tìm số chia hết cho x đầu tiên có trong mảng
+ * @params {array} array - Mảng các chữ số
+ * @params {number} x - Số bị chia
+ * Nếu không có trả về undefined
+ * Example: 
+ * findDivisibleNum([1, 3, 4], 2) => 4
+ */
+
+function findDivisibleNum(array, x) {
+  // viết code ở đây.
+  return array.find(function(num){
+      return num % x === 0;
+  })
+}
+
+//----------  finish  ----------
