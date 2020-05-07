bd0cd53bc309006eabb497da2215d00391aedeff
NHN <58180674+nhn6298@users.noreply.github.com>
2020-05-07 10:42:59 +0700

    Create unit-06.js

---
+++ unit-06.js
@@ -0,0 +1,25 @@
+/**
+ * Cách sử dụng Array và Object khác nhau như thế nào. Viết câu trả lời ở phía dưới.
+ */
+// Object là để khai báo 1 đối tượng 
+// Array để khai báo một mảng gồm nhiều đối tượng
+------ Bài 2 --------
+/*
+Khai báo một array "listAnimal" chứa các object mô tả danh sách các con vật.
+Mỗi con vật có 2 thuộc tính: name và age trong đó name có kiểu string, age có kiểu number.
+ * {name}: string;
+ * {age}: number;
+*/
+var dachshund = {
+  name: 'lap xuong',
+  age: 10
+};
+var labrador = {
+  name: 'Gau dan',
+  age: 8
+};
+var shiba = {
+  name: 'Momo',
+  age: 7
+};
+var listAnimal = [dachshund, labrador, shiba];
