db108d7498f01eaaa5c5f248295fedb818259a2b
NHN <58180674+nhn6298@users.noreply.github.com>
2020-05-07 10:51:11 +0700

    Create unit-09.js

---
+++ unit-09.js
@@ -0,0 +1,13 @@
+//-------- exercise 1 ----------
+**
+ * Viết ra cách để tăng a lên 2 đơn vị (1 trong các cách trong phạm vi bài học)
+ */
+ var a = 10;
+ a += 2;
+//-------- exercise 2 ----------
+/**
+ * Viết ra một trong các cách khác nhau có thể (trong phạm vi bài học) để gấp đôi a
+ */
+ var a = 10;
+ a *= 2;
+//----------  finish  ----------
