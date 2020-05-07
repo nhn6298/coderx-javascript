88a99662c5d99a62d6e6f31932220f4dc089814b
NHN <58180674+nhn6298@users.noreply.github.com>
2020-05-07 11:06:56 +0700

    Create unit-11.js

---
+++ unit-11.js
@@ -0,0 +1,22 @@
+//-------- exercise 1 ----------
+/**
+ * Khai báo biến rectangle là 1 object gồm có các property:
+ * - width: chiều rộng
+ * - height: chiều dài
+ * - getWidth: trả về chiều rộng
+ * - getHeight: trả về chiều dài
+ * - getArea: trả về diện tích
+ */
+var rectangle = {
+  width: 10,
+  height: 20}
+  function getWidth(rectangle){ 
+      return rectangle.width;
+  }
+  function getHeight(rectangle){ 
+      return rectangle.height;
+  }
+  function getArea(rectangle){ 
+      return  rectangle.height * rectangle.width;
+  }
+//----------  finish  ----------
